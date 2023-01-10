4be0254c150d522a2cddb3a1be3b8be784d8a2c1
jayshah28 <jayshah9791@example.com>
2023-01-10 18:08:07 +0530

    dummy check

--- src/Components/Dashboard/Main.js
+++ src/Components/Dashboard/Main.js
@@ -124,7 +124,7 @@ const Main = () => {
             title="Click to see Monthly Trip details"
             onClick={() => history.push("/trips")}
           >
-            <p>Trips</p>
+            <p>Org Trips</p>
             {isApiError && (
               <span style={{ fontWeight: "normal", fontSize: "14px" }}>
                 {isApiError}
