9a1a6a836ebbc6fbd20e223c3d2d89d56873f48c
jayshah2804 <jayshah9791@gmail.com>
2023-04-08 16:02:26 +0530

    change

--- src/Components/PrivateDriver/DriverBooking.js
+++ src/Components/PrivateDriver/DriverBooking.js
@@ -146,7 +146,7 @@ const DriverBooking = (props) => {
     if (isDriverBookingClicked) {
       move();
       paraMeters();
-      fetch(url, requestOptions)
+      fetch("https://corp.little.global/server" + url, requestOptions)
         .then((response) => response.text())
         .then((result) => {
           setIsToken(JSON.parse(result).token);
@@ -156,7 +156,7 @@ const DriverBooking = (props) => {
         .catch((error) => console.log("error", error));
     } else if (isToken) {
       paraMeters();
-      fetch(url, requestOptions)
+      fetch("https://corp.little.global/server" + url, requestOptions)
         .then((response) => response.text())
         .then((result) => {
           JSON.parse(result).tripId
